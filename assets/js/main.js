457c0baf60638674531aed27a61c38ee55591bf4
harrismalic <harrismalik037@gmail.com>
2022-01-31 21:44:12 +0500

    pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,22 +18,4 @@ $.getJSON("./data/products.json", function (data) {
     });
   });
 
-  $.getJSON("./data/products.json", function (data) {
-    data.map((product) => {
-      $("#con-product").append(
-        "<div class='col-md-3'>" +
-          "<div class='card' style=' background: url(./assets/images/banner.jpg); background-size: cover; height: 350px;'>" +
-          "<div class='card-body' style='text-align: center; position: relative; top: 40%'>" +
-          "<a href='#' style='display: none' class='btn btn-secondary'>Go somewhere</a>" +
-          "</div>" +
-          "</div>" +
-          "<div style='margin-top: 10px'> <p>" +
-          product.productName +
-          "</p> <div style='display:flex'><p style='padding-right:10px' class='text-danger'>$" +
-          product.price +
-          "</p><del>" +
-          product.salePrice +
-          "</del></div> </div></div>"
-      );
-    });
-  });
+  
